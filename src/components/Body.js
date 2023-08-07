f8a8bb2c0d330b47da939531b6efa0f5fdf25a2f
priyankas0907 <priyankasinha0301@gmail.com>
2023-08-07 08:34:08 +0530

    useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockdata";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  [listOfRes, setListOfRes] = useState(resList);
+  [listOfRes, setListOfRes] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/mapi/homepage/getCards?lat=12.8740739&lng=77.6440622"
+    );
+    console.log(data);
+    const jsondata = await data.json();
+    console.log(resList);
+    console.log(jsondata.data.success.cards[1].gridWidget.gridElements.infoWithStyle.restaurants);
+    setListOfRes(jsondata?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+   
+  };
   return (
     <div className="body">
       <div className="filter">
