92055e810318dbd4c7ef2770a7c4761a99913015
priyankas0907 <priyankasinha0301@gmail.com>
2023-08-21 23:11:11 +0530

    Lifting the state up

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
+// import { useState } from "react";
 import ItemsList from "./ItemsList";
 
-const RestaurantCategory = ({ data }) => {
-  const [showItems, setShowItems] = useState(false);
+const RestaurantCategory = ({ data , showItem, setShowIndex}) => { //3. passing the prop callback fn
+//   const [showItems, setShowItems] = useState(false);
   const handleClick = () => {
-   setShowItems(!showItems);
+    setShowIndex(); //5. exceute callback from here
   };
   return (
     //header
@@ -12,7 +12,7 @@ const RestaurantCategory = ({ data }) => {
       <div className="w-6/12 mx-auto  my-4 shadow-lg bg-gray-50 p-4 ">
         <div
           className="flex justify-between cursor-pointer"
-          onClick={handleClick}
+          onClick={handleClick} //4. Create local handleclick
         >
           <span className="font-bold text-lg">
             {data.title} ({data.itemCards.length})
@@ -20,7 +20,7 @@ const RestaurantCategory = ({ data }) => {
           <span>⬇️</span>
         </div>
 
-        {showItems && <ItemsList items={data.itemCards} />}
+        {showItem && <ItemsList items={data.itemCards} />}
       </div>
     </div>
   );
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,32 +1,48 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import Shimmer from "./Shimmer";
 import RestaurantCategory from "./RestaurantCategory";
-console.log(Shimmer)
 
+console.log(Shimmer);
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
 
   const resInfo = useRestaurantMenu(resId);
+  const [showIndex, setShowIndex] = useState(null);//1.
+  if (resInfo === null) return <Shimmer />;
 
-   if (resInfo === null ) return (<Shimmer />);
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.data?.cards[0]?.card?.card?.info;
+  // console.log(resInfo?.data?.cards[2]);
+  // const {itemCards} = resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+  // console.log(itemCards);
+  console.log("resInfo1");
+  console.log(resInfo?.data?.cards[3]);
 
-const {name, cuisines,costForTwoMessage} = resInfo?.data?.cards[0]?.card?.card?.info;
-// console.log(resInfo?.data?.cards[2]);
-// const {itemCards} = resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-// console.log(itemCards);
-console.log("resInfo1");
-console.log(resInfo?.data?.cards[3]);
-
-const categories = resInfo?.data?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c=> c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
-console.log(categories);
+  const categories =
+    resInfo?.data?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  console.log(categories);
 
   return (
     <div className="text-center">
       <h1 className="font-bold m-6 text-2xl">{name}</h1>
-      <p className="font-bold">{cuisines.join(" ,")} - {costForTwoMessage}</p>
-      {categories.map((category)=>(<RestaurantCategory key={category?.card?.card?.title} data={category?.card?.card}/>))}
+      <p className="font-bold">
+        {cuisines.join(" ,")} - {costForTwoMessage}
+      </p>
+      {categories.map((category, index) => (
+        <RestaurantCategory
+          key={category?.card?.card?.title}
+          data={category?.card?.card}
+          showItem={index == showIndex ? true : false}
+          setShowIndex={()=> setShowIndex(index)}//2.
+        />
+      ))}
       {/* <h2>Menu</h2>
       <ul>
         {itemCards.map((item)=>(
